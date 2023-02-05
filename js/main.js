16b1539543374ddf8147a50536998a689e15e00b
Zaid Jamous <zaidjam512@gmail.com>
2023-02-05 17:42:07 +0200

    second commit

--- js/main.js
+++ js/main.js
@@ -102,25 +102,21 @@ let swiperPortfolio = new Swiper('.portfolio__container', {
   },
 
 });
-
-/*==================== TESTIMONIAL ====================*/
-let swiperTestimonial = new Swiper('.testimonial__container', {
-  loop: true,
-  grabCursor : true,
-  spaceBetween: 48,
-
-  pagination: {
-    el: ".swiper-pagination",
-    clickable : true,
-    dynamicBullets : true,
-  },
-  breakpoints:{
-    568:{
-      slidesPerView: 2,
-    }
-  }
-});
-
+/*==================== Contact Send Message ====================*/
+window.onload = function() {
+  document.getElementById('contact-form').addEventListener('submit', function(event) {
+      event.preventDefault();
+      // generate a five digit number for the contact_number variable
+      this.contact_number.value = Math.random() * 100000 | 0;
+      // these IDs from the previous steps
+      emailjs.sendForm('contact_service', 'contact_form', this)
+          .then(function() {
+              console.log('SUCCESS!');
+          }, function(error) {
+              console.log('FAILED...', error);
+          });
+  });
+}
 /*==================== SCROLL SECTIONS ACTIVE LINK ====================*/
 const sections = document.querySelectorAll('section[id]')
 
