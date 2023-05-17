d4086eb46f1682381d40b4cbf214336035897e1e
Zaid Jamous <zaidjam512@gmail.com>
2023-05-17 20:25:45 +0300

    fifth

--- js/main.js
+++ js/main.js
@@ -122,9 +122,14 @@ xhr.onload = function() {
         },
       },
     });
+    swiper.slideTo(2);   // Navigates to the third slide
+    swiper.update();     // Updates the Swiper instance
+    var activeSlide = swiper.slides[swiper.activeIndex]; 
   }
 };
+
 xhr.send();
+
 /*==================== Contact Send Message ====================*/
 window.onload = function() {
   document.getElementById('contact-form').addEventListener('submit', function(event) {
